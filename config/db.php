120356f4b176d482a884963d60544df411600457
Kinga <knapik.kinga@gmail.com>
2015-02-08 17:16:23 +0100

    vendor

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=konwent',
+    'username' => 'p',
     'password' => '',
     'charset' => 'utf8',
 ];
